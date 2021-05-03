8878ec4e4d57065d1d64d9ca035a7703733f95a1
sandordeli <uba.sandor.deli@ext.ewerk.com>
2021-05-03 15:06:22 +0200

    feat(app): ready for dev
    - base configs, entities, modules, migration, seed data done

---
+++ migrations/1619949161505-BootstrapTables.ts
@@ -0,0 +1,81 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class BootstrapTables1619949161505 implements MigrationInterface {
+  name = 'BootstrapTables1619949161505';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "actor" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, CONSTRAINT "PK_05b325494fcc996a44ae6928e5e" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "director" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, CONSTRAINT "PK_b85b179882f31c43324ef124fea" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "genre" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, CONSTRAINT "PK_0285d4f1655d080cfcf7d1ab141" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "movie" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "description" character varying NOT NULL, "rating" integer NOT NULL, "directorId" integer, CONSTRAINT "PK_cb3bb4d61cf764dc035cbedd422" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "movie_actors_actor" ("movieId" integer NOT NULL, "actorId" integer NOT NULL, CONSTRAINT "PK_a69e570bd35d7cd2139d12270e9" PRIMARY KEY ("movieId", "actorId"))`,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_992f9af300d8c96c46fea4e541" ON "movie_actors_actor" ("movieId") `,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_65be8ded67af2677acfd19854c" ON "movie_actors_actor" ("actorId") `,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "movie_genres_genre" ("movieId" integer NOT NULL, "genreId" integer NOT NULL, CONSTRAINT "PK_aee18568f9fe4ecca74f35891af" PRIMARY KEY ("movieId", "genreId"))`,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_985216b45541c7e0ec644a8dd4" ON "movie_genres_genre" ("movieId") `,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_1996ce31a9e067304ab168d671" ON "movie_genres_genre" ("genreId") `,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "movie" ADD CONSTRAINT "FK_a32a80a88aff67851cf5b75d1cb" FOREIGN KEY ("directorId") REFERENCES "director"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "movie_actors_actor" ADD CONSTRAINT "FK_992f9af300d8c96c46fea4e5419" FOREIGN KEY ("movieId") REFERENCES "movie"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "movie_actors_actor" ADD CONSTRAINT "FK_65be8ded67af2677acfd19854c2" FOREIGN KEY ("actorId") REFERENCES "actor"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "movie_genres_genre" ADD CONSTRAINT "FK_985216b45541c7e0ec644a8dd4e" FOREIGN KEY ("movieId") REFERENCES "movie"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "movie_genres_genre" ADD CONSTRAINT "FK_1996ce31a9e067304ab168d6715" FOREIGN KEY ("genreId") REFERENCES "genre"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "movie_genres_genre" DROP CONSTRAINT "FK_1996ce31a9e067304ab168d6715"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "movie_genres_genre" DROP CONSTRAINT "FK_985216b45541c7e0ec644a8dd4e"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "movie_actors_actor" DROP CONSTRAINT "FK_65be8ded67af2677acfd19854c2"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "movie_actors_actor" DROP CONSTRAINT "FK_992f9af300d8c96c46fea4e5419"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "movie" DROP CONSTRAINT "FK_a32a80a88aff67851cf5b75d1cb"`,
+    );
+    await queryRunner.query(`DROP INDEX "IDX_1996ce31a9e067304ab168d671"`);
+    await queryRunner.query(`DROP INDEX "IDX_985216b45541c7e0ec644a8dd4"`);
+    await queryRunner.query(`DROP TABLE "movie_genres_genre"`);
+    await queryRunner.query(`DROP INDEX "IDX_65be8ded67af2677acfd19854c"`);
+    await queryRunner.query(`DROP INDEX "IDX_992f9af300d8c96c46fea4e541"`);
+    await queryRunner.query(`DROP TABLE "movie_actors_actor"`);
+    await queryRunner.query(`DROP TABLE "movie"`);
+    await queryRunner.query(`DROP TABLE "genre"`);
+    await queryRunner.query(`DROP TABLE "director"`);
+    await queryRunner.query(`DROP TABLE "actor"`);
+  }
+}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,189 @@
-import { Header, Injectable } from '@nestjs/common';
+import { Genre } from './entities/genre.entity';
+import { SeedMovie } from './movie.interfaces';
+import { Movie } from './entities/movie.entity';
+import { Actor } from './entities/actor.entity';
+import { Director } from './entities/director.entity';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { readFileSync } from 'fs';
+
+export interface CreateMovieEntityOpts {
+  seedMovie: SeedMovie;
+  actors: Actor[];
+  directors: Director[];
+  genres: Genre[];
+}
 
 @Injectable()
 export class AppService {
-  @Header('content-type', 'text/plain')
+  constructor(
+    @InjectRepository(Director)
+    private readonly directorRepo: Repository<Director>,
+    @InjectRepository(Actor)
+    private readonly actorRepo: Repository<Actor>,
+    @InjectRepository(Genre)
+    private readonly genreRepo: Repository<Genre>,
+    @InjectRepository(Movie)
+    private readonly moveRepo: Repository<Movie>,
+  ) {}
   getHello(): any {
     console.log('sannya');
     return { sanya: 'Hello World!' };
   }
+
+  seedInitialDirectors() {
+    const directorNames = [
+      'Quentin Tarantino',
+      'Michael Matthews',
+      'Ben Falcone',
+      'James Gunn',
+      'Anders Thomas Jensen',
+      'David Fincher',
+      'Don Hall, Carlos López Estrada, Paul Briggs, John Ripa',
+      'Thomas Vinterberg',
+      'Max Barbakow',
+      'Pete Docter, Kemp Powers',
+      'J Blakeson',
+      'Patrick Hughes',
+    ];
+
+    const directors = directorNames.map((currDirectorName) => {
+      const director = new Director();
+      director.name = currDirectorName;
+      return director;
+    });
+
+    return this.directorRepo.save(directors);
+  }
+
+  async seedInitialActors() {
+    const actorNames = [
+      "Dylan O'Brien",
+      'Jessica Henwick',
+      'Michael Rooker',
+      'Dan Ewing',
+      'Melissa McCarthy',
+      'Octavia Spencer',
+      'Jason Bateman',
+      'Bobby Cannavale',
+      'Margot Robbie',
+      'Idris Elba',
+      'John Cena',
+      'Mads Mikkelsen',
+      'Nikolaj Lie Kaas',
+      'Andrea Heick Gadeberg',
+      'Lars Brygmann',
+      'Gary Oldman',
+      'Amanda Seyfried',
+      'Lily Collins',
+      'Tom Pelphrey',
+      'Kelly Marie Tran',
+      'Awkwafina',
+      'Gemma Chan',
+      'Izaac Wang',
+      'Thomas Bo Larsen',
+      'Magnus Millang',
+      'Lars Ranthe',
+      'Andy Samberg',
+      'Cristin Milioti',
+      'J.K. Simmons',
+      'Peter Gallagher',
+      'Jamie Foxx',
+      'Tina Fey',
+      'Graham Norton',
+      'Rachel House',
+      'Leonardo DiCaprio',
+      'Brad Pitt',
+      'Emile Hirsch',
+      'Rosamund Pike',
+      'Peter Dinklage',
+      'Eiza González',
+      'Dianne Wiest',
+      'Ryan Reynolds',
+      'Frank Grillo',
+      'Salma Hayek',
+      'Samuel L. Jackson',
+    ];
+
+    const actors = actorNames.map((currDirectorName) => {
+      const director = new Actor();
+      director.name = currDirectorName;
+      return director;
+    });
+
+    return this.actorRepo.save(actors);
+  }
+
+  seedInitialGenres() {
+    const genreNames = [
+      'Action',
+      'Adventure',
+      'Comedy',
+      'Drama',
+      'Animation',
+      'Short',
+      'Romance',
+      'Musical',
+      'Crime',
+      'Sport',
+      'Sci-Fi',
+      'Biography',
+      'Fantasy',
+      'Mystery',
+      'Family',
+      'Thriller',
+    ];
+
+    const genres = genreNames.map((currDirectorName) => {
+      const director = new Director();
+      director.name = currDirectorName;
+      return director;
+    });
+
+    return this.genreRepo.save(genres);
+  }
+
+  async seedInitialMovies(
+    directors: Director[],
+    actors: Actor[],
+    genres: Genre[],
+  ) {
+    const moviesStr = readFileSync('./assets/movies.json', 'utf-8');
+    const seedMovies = JSON.parse(JSON.parse(moviesStr)) as SeedMovie[];
+
+    const movies = seedMovies.map((seedMovie) => {
+      return createMovieEntity({
+        directors,
+        actors,
+        genres,
+        seedMovie,
+      });
+    });
+
+    return this.moveRepo.save(movies);
+  }
+}
+
+function createMovieEntity(params: CreateMovieEntityOpts) {
+  const { seedMovie, genres, actors, directors } = params;
+  const genresOfCurrMov = genres.filter((currGenre) =>
+    seedMovie.genres.includes(currGenre.name),
+  );
+
+  const actorsOfCurrMov = actors.filter((currActor) => {
+    return seedMovie.stars.includes(currActor.name);
+  });
+
+  const directorOfCurrMov = directors.find(
+    (currDirector) => currDirector.name === seedMovie.director,
+  );
+
+  const movie = new Movie();
+  movie.description = seedMovie.description;
+  movie.rating = parseInt(seedMovie.rating);
+  movie.actors = actorsOfCurrMov;
+  movie.genres = genresOfCurrMov;
+  movie.director = directorOfCurrMov;
+  return movie;
 }
